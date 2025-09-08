4ca08c1f3acabe7e8bcfdc43b50a058ecc062a43
sakanataro0113 <fuku.sita2005@gmail.com>
2025-09-08 16:50:20 +0900

    ライトモード適用

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -80,7 +80,7 @@ export default function PostForm(){
 
             <button
                 type='submit'
-                className='appearance-none bg-blue-500 text-white py-2 rounded hover:bg-blue-600'
+                className='force-color appearance-none bg-blue-500 text-white py-2 rounded hover:bg-blue-600'
             >
             投稿
             </button>
