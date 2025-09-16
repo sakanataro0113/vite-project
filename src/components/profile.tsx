2bc9641a42c5c0fe3e70cb75fd10b964536f7c1c
sakanataro0113 <fuku.sita2005@gmail.com>
2025-09-16 15:45:33 +0900

    プロフ編集

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -4,7 +4,7 @@ export default function Profile(){
             <h2>ブログ執筆者紹介</h2>
             <p>年齢：20歳</p>
             <p>職業：学生(情報系大学生)</p>
-            <p>趣味：ゲーム、アニメ、温泉巡り、=LOVE(new)</p>
+            <p>趣味：ゲーム、アニメ、温泉巡り</p>
         </div>
     )
 }
