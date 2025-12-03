27c2523422604bbd5f42fb8eb4881f84b073a74a
fukutaro <fuku.sita2005@gmail.com>
2025-12-04 00:37:05 +0900

    リンク適用

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -6,7 +6,16 @@ export default function Profile(){
             <p>職業：学生(情報系大学生)</p>
             <p>趣味：猫、ゲーム、アニメ、温泉巡り</p>
             <p>アルバイト：パン屋、qgisシステム開発</p>
-            <p>github: https://github.com/sakanataro0113</p>
+            <p>github: 
+                <a 
+                    href="https://github.com/sakanataro0113"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500 hover:underline"
+                >
+                    https://github.com/sakanataro0113
+                </a>
+            </p>
         </div>
     )
 }
